1e0583597bbaa6df398f77f9c1ea6070863354dc
psh2003 <83701900+psh2003@users.noreply.github.com>
2023-10-09 22:02:12 +0900

    first commit

---
+++ src/main/java/com/pbl/pt/pt_management_web/PtManagementWebApplication.java
@@ -0,0 +1,13 @@
+package com.pbl.pt.pt_management_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtManagementWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtManagementWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pbl/pt/pt_management_web/PtManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.pt.pt_management_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtManagementWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
